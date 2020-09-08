027d714f46f039f80876ee75ebbdd3b8b2e66ac2
Edward Lanto <elanto@hotmail.com>
2020-09-07 19:57:36 -0700

    added page click and selections for links

--- pageController.js
+++ pageController.js
@@ -1,20 +1,10 @@
 const pageScraper = require('./pageScraper');
-const fs = require('fs');
 async function scrapeAll(browserInstance){
     let browser;
     try{
         browser = await browserInstance;
-        let scrapedData = {};
-        // Call the scraper for different set of books to be scraped
-        scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
-        
-        fs.writeFile("data.json", JSON.stringify(scrapedData), 'utf8', function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("The data has been scraped and saved successfully! View it at './data.json'");
-        });
-        await browser.close();
+        await pageScraper.scraper(browser);
+
     }
     catch(err){
         console.log("Could not resolve the browser instance => ", err);
